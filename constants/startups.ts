7178e1ea5b21d10225236d293e23ad740c0c13b5
minhy <kasamin98@naver.com>
2025-11-04 11:22:41 +0900

    feat: StartupSection 컴포넌트에 링크 추가 - startup 카드 상수값에 websiteUrl추가 - startup 카드 타입에 websiteUrl추가

--- constants/startups.ts
+++ constants/startups.ts
@@ -10,6 +10,7 @@ export const STARTUPS: Startup[] = [
     description:
       "멋쟁이사자처럼 대학에서 만난 개발자 친구들과 창업 네트워크를 기반으로 만든 재능 공유 플랫폼 1등!\n탈일을 소개 합니다!",
     logoUrl: "/landing/startups/taling.png",
+    websiteUrl: "https://www.taling.me/",
   },
   {
     id: 2,
@@ -19,6 +20,7 @@ export const STARTUPS: Startup[] = [
     description:
       "런칭 1년 반 만에 MAU 100만 찍고,\n어마어마한 속도로 투자까지 받아 850만 X세대 남성의 소비문화를 뒤흔들고 있는 바로 그 브랜드, 애슬러를 소개합니다.",
     logoUrl: "/landing/startups/athler.png",
+    websiteUrl: "https://athler.kr/",
   },
   {
     id: 3,
@@ -28,6 +30,7 @@ export const STARTUPS: Startup[] = [
     description:
       "멋쟁이 사자처럼 대학 아이디어톤 2등,\n해커톤 1등 연속 수상 후 바로 창업에 \n뛰어들어, 지금은 국내 1위 요약 AI\n서비스가 된 곳, 바로 릴리스 AI입니다!",
     logoUrl: "/landing/startups/lilys.png",
+    websiteUrl: "https://lilys.ai/",
   },
   {
     id: 4,
@@ -37,6 +40,7 @@ export const STARTUPS: Startup[] = [
     description:
       '멋쟁이사자처럼 대학 활동을 통해 "이젠\n뭐든 만들 수 있겠다!"는 확신을 얻고, 그 터닝포인트 덕분에 더 똑똑하게 일하는\n세상을 위한 SaaS 관리 서비스를 만든 팀,\n제로원퍼블릭이에요!',
     logoUrl: "/landing/startups/scodi.png",
+    websiteUrl: "https://www.scordi.io/",
   },
   {
     id: 5,
@@ -46,6 +50,7 @@ export const STARTUPS: Startup[] = [
     description:
       "멋쟁이사자처럼 대학에서 프로덕트 10개\n넘게 만들다가, 멋쟁이사자처럼 대학 동기랑 창업까지 하게 된 결과물은?\n신입, 주니어 대표 취업 플랫폼, \n자소설닷컴!",
     logoUrl: "/landing/startups/jasoseol.png",
+    websiteUrl: "https://jasoseol.com/",
   },
   {
     id: 6,
@@ -55,6 +60,7 @@ export const STARTUPS: Startup[] = [
     description:
       '"비즈니스가 존재하는 한 사라지지 않는"\n사람과 사람, 기업과 기업의 연결을 더\n간편하게 만드는 B2B SaaS를 통해글로벌\n서비스로 거듭난 스플랩을 소개합니다!',
     logoUrl: "/landing/startups/splab.png",
+    websiteUrl: "https://umoh.io/ko/splab",
   },
   {
     id: 7,
@@ -64,6 +70,7 @@ export const STARTUPS: Startup[] = [
     description:
       "채용 리소스를 확 줄여주는 채용 솔루션으로 기업과 투자자들의 마음을 사로잡은\n그리팅을 소개합니다!",
     logoUrl: "/landing/startups/greeting.png",
+    websiteUrl: "https://www.greetinghr.com/",
   },
   {
     id: 8,
@@ -73,6 +80,7 @@ export const STARTUPS: Startup[] = [
     description:
       "아마추어부터 프로까지 모든 선수와 팀의\n성장을 위한 카메라 및 분석 프로그램,\n데이터를 제공하는 글로벌 플랫폼\n비프로일레븐도 멋사 출신이에요!",
     logoUrl: "/landing/startups/bepro11.png",
+    websiteUrl: "https://www.bepro.ai/",
   },
   {
     id: 9,
@@ -82,5 +90,6 @@ export const STARTUPS: Startup[] = [
     description:
       "멋쟁이사자처럼 대학에서 개발 시작해서\n\"일단 한 번 해보자!\" 하고 달렸떠니,\n창업가들의 등용문 '정주영창업경진대회'에서 대상까지 받은 팀, \n바로 허밍버즈예요!",
     logoUrl: "/landing/startups/babywhale.png",
+    websiteUrl: "https://www.babywhale.io/",
   },
 ];

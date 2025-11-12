10833ae29495a01aadc6b2cea683966ef46142d3
minhy <kasamin98@naver.com>
2025-11-12 19:19:22 +0900

    refactor: globals.css 중복 import 수정

--- app/(base)/layout.tsx
+++ app/(base)/layout.tsx
@@ -1,8 +1,6 @@
-import "../globals.css";
 import Footer from "@/components/Footer";
 
 export default function BaseLayout({ children }: { children: React.ReactNode }) {
-  // ✅ html/body 없음, body 내부에 삽입되는 레이아웃
   return (
     <>
       {children}

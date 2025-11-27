caa3e5136dc1090b3fd020918f9b32d8e0cd7412
minhy <kasamin98@naver.com>
2025-11-28 01:18:10 +0900

    feat: Toast 알림 시스템 구현

---
+++ store/toastStore.ts
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+
+interface ToastState {
+  isVisible: boolean;
+  message: string;
+  showToast: (message: string) => void;
+  hideToast: () => void;
+}
+
+export const useToastStore = create<ToastState>((set) => ({
+  isVisible: false,
+  message: "",
+  showToast: (message: string) => set({ isVisible: true, message }),
+  hideToast: () => set({ isVisible: false, message: "" }),
+}));
